77f6db165992d7c87107eb2c0af14498b46daca9
Mikhail Panshenskov <mpansh@gmail.com>
2016-09-15 05:01:11 +0200

    Initial project

---
+++ src/main/java/TelegramBotMessageSender.java
@@ -0,0 +1,137 @@
+import com.pengrad.telegrambot.TelegramBot;
+import com.pengrad.telegrambot.TelegramBotAdapter;
+import com.pengrad.telegrambot.request.SendMessage;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDateTime;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static java.nio.file.Files.write;
+import static java.util.Arrays.asList;
+import static java.util.Collections.singletonList;
+import static java.util.stream.Collectors.groupingBy;
+import static org.apache.commons.lang3.StringUtils.split;
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static java.nio.file.StandardOpenOption.APPEND;
+import static java.nio.file.StandardOpenOption.CREATE;
+
+/**
+ * @author mipansh
+ *         Date: 2016/09/11
+ */
+public class TelegramBotMessageSender {
+
+    private static final Logger logger = LogManager.getLogger(TelegramBotMessageSender.class);
+    public static final Path MESSAGES_FILE_PATH = Paths.get(System.getProperty("MESSAGES_FILE_PATH", "bot.msg"));
+    public static final Path IMPORT_FILE_PATH = Paths.get(System.getProperty("IMPORT_FILE_PATH", "wiki.pages.csv"));
+
+
+    public static final String BASEURL = "https://api.telegram.org/bot";
+    public static final String TOKEN = "248586768:AAFY8ebEYjlyq0DuBzVbQnEvC5NDwoP3eK0";
+
+    public static final Map<String, String> AVAILABLE_CATEGORIES = new LinkedHashMap<String, String>() {{
+        put("en","List_of_English_writers");
+        put("fr","List_of_French-language_authors");
+        put("de","List_of_German-language_authors");
+        put("ru","List_of_Russian-language_writers");
+    }};
+
+    public static void main(String[] args) throws IOException {
+        List<WikiCatalogExport.WikiPage> wikiPages = new ArrayList<>();
+        try (Stream<String> stream = Files.lines(IMPORT_FILE_PATH)) {
+            stream.forEach(line -> wikiPages.add(WikiCatalogExport.WikiPage.fromString(line)));
+        }
+        Map<String, TreeSet<WikiCatalogExport.WikiPage>> rankedPages = wikiPages.stream().
+                collect(groupingBy(WikiCatalogExport.WikiPage::getCategory, Collectors.toCollection(TreeSet::new)));
+
+        int dayOfMonth = LocalDateTime.now().getDayOfMonth();
+        String lang = new ArrayList<>(AVAILABLE_CATEGORIES.keySet()).get(dayOfMonth % 4);
+        logger.info("Language of the day {} is {}", dayOfMonth, lang);
+
+        String category = AVAILABLE_CATEGORIES.get(lang);
+        WikiCatalogExport.WikiPage page = getHighestNotYetSent(rankedPages.get(category));
+        String url = getInNativeLanguageIfPossible(lang, page);
+        logger.info("Wiki page of the day in " + lang + " : " + url);
+
+        sendMessage(url);
+        String message = LocalDateTime.now() + "|" + page.getId() + "|" + url;
+        logger.info(message);
+        write(MESSAGES_FILE_PATH, singletonList(message), UTF_8, APPEND, CREATE);
+    }
+
+    private static WikiCatalogExport.WikiPage getHighestNotYetSent(TreeSet<WikiCatalogExport.WikiPage> rankedPages) throws IOException {
+        final HashSet<String> messageParts = readMessageParts();
+
+        // check if page has already been sent
+        WikiCatalogExport.WikiPage highest = rankedPages.last();
+        while (messageParts.contains(highest.getId())) {
+            rankedPages.pollLast();
+            highest = rankedPages.last();
+        }
+
+        return highest;
+    }
+
+    private static HashSet<String> readMessageParts() throws IOException {
+        try {
+            Files.createFile(MESSAGES_FILE_PATH);
+        } catch (IOException ignore) {
+        }
+        List<String> messages = Files.readAllLines(MESSAGES_FILE_PATH);
+        final HashSet<String> messageParts = new HashSet<>();
+        messages.stream().forEach(line -> {
+            messageParts.addAll(asList(split(line, '|')));
+        });
+        return messageParts;
+    }
+
+    private static String getInNativeLanguageIfPossible(String lang, WikiCatalogExport.WikiPage page) throws IOException {
+        String url = "https://" + page.getUrl();
+        if ("en".equals(lang) && url.contains("en.wikipedia"))
+            return url;
+
+        Document doc = Jsoup.connect(url).get();
+        try {
+            Elements nativeLink = doc.select("div#p-lang > div > ul > li > a[lang=" + lang + "]");
+            url = nativeLink.get(0).attr("href");
+        } catch (Exception ignore) {
+            logger.info("Could not find page in native language '{}': {}", url);
+        }
+        return url;
+    }
+
+
+    private static void sendMessage(String message) {
+        TelegramBot bot = TelegramBotAdapter.build(TOKEN);
+        bot.execute(new SendMessage(59323870, message));
+        bot.execute(new SendMessage(295144283, message));
+    }
+
+    static class Person {
+        private String name;
+        private int age;
+
+        Person(String name, int age) {
+
+            this.name = name;
+            this.age = age;
+        }
+
+        @Override
+        public String toString() {
+            return String.format("Person{name='%s', age=%d}", name, age);
+        }
+    }
+
+
+}
---
+++ src/main/java/WikiCatalogExport.java
@@ -0,0 +1,244 @@
+import junit.framework.Assert;
+import lombok.Builder;
+import lombok.Data;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+import static java.lang.String.format;
+import static java.lang.String.valueOf;
+import static java.util.stream.Collectors.summingInt;
+import static java.util.stream.Collectors.toList;
+import static org.apache.logging.log4j.LogManager.getLogger;
+
+/**
+ * Wiki catalog utility class that fetches wiki catalog from the Wiki website and exports it into the file.
+ *
+ * Examples of catalogs to fetch:
+ *   * https://en.wikipedia.org/wiki/List_of_French-language_authors
+ *   * https://en.wikipedia.org/wiki/List_of_Russian-language_writers
+ *   * https://en.wikipedia.org/wiki/List_of_German-language_authors
+ *   * https://en.wikipedia.org/wiki/List_of_English_writers
+ */
+public class WikiCatalogExport {
+
+    public static void main(String[] args) throws IOException {
+        WikiCatalog wikiCatalog = new WikiCatalog();
+        Predicate<Element> filterOutPredicate = el -> {
+            String link = el.attr("href").toLowerCase();
+            return !link.contains("list_of") && !link.contains("russian_") && !link.contains("literature");
+        };
+        wikiCatalog.updateCatalog("en.wikipedia.org", "List_of_Russian-language_writers", "div#mw-content-text > ul > li > a:first-child", filterOutPredicate);
+        wikiCatalog.updateCatalog("en.wikipedia.org", "List_of_English_writers", "div#mw-content-text > div > ul > li > a", filterOutPredicate);
+        wikiCatalog.updateCatalog("en.wikipedia.org", "List_of_German-language_authors", "div#mw-content-text > dl > dd > a", filterOutPredicate);
+        wikiCatalog.updateCatalog("en.wikipedia.org", "List_of_French-language_authors", "div#mw-content-text > ul > li > a:first-child", filterOutPredicate);
+
+        wikiCatalog.exportToFile(System.getProperty("PATH_TO_EXPORT", "wiki.pages.csv"));
+    }
+
+    @Data
+    public static class WikiCatalog {
+        private static final Logger log = LogManager.getLogger(WikiCatalog.class);
+
+        List<WikiPage> pages = new ArrayList<>();
+
+        public void updateCatalog(String project, String category, String cssQuery, Predicate<Element> filterOutPredicate)  {
+            List<WikiPage> wikiPages = retrieveCatalog(project, category, cssQuery, filterOutPredicate);
+            pages.addAll(wikiPages);
+        }
+
+        public void exportToFile(String filePath) {
+            List<String> wikiPages = pages.stream().map(WikiPage::toString).collect(toList());
+            log.info("Export {} wiki pages.", wikiPages.size());
+            try {
+                Files.write(Paths.get(filePath), wikiPages);
+            } catch (IOException e) {
+                throw new RuntimeException("Could not export wiki-pages to file: " + filePath);
+            }
+        }
+
+        public static List<WikiPage> retrieveCatalog(String project, String category, String cssQuery, Predicate<Element> filterOutPredicate)  {
+            List<WikiPage> pages = new ArrayList<>();
+
+            // select elements for wiki project under the given category
+            String wikiCategoryPageUrl = "https://" + project + "/wiki/" + category;
+            Document doc;
+            try {
+                doc = Jsoup.connect(wikiCategoryPageUrl).get();
+            } catch (IOException e) {
+                throw new RuntimeException("Could not retrieve catalog from: " + wikiCategoryPageUrl, e);
+            }
+            Elements elements = doc.select(cssQuery);
+
+            // filter out elements
+            List<Element> filteredElements = elements.stream().filter(filterOutPredicate).collect(toList());
+            log.info("Retrieved[{}] - filtered-out[{}] = {} wiki-links from wiki-category url: {}", elements.size(), (elements.size()-filteredElements.size()), filteredElements.size(), wikiCategoryPageUrl);
+            if (log.isDebugEnabled()) {
+                filteredElements.stream().map(el -> "https://en.wikipedia.org" + el.attr("href")).
+                        forEach(url -> { log.debug("Page: {}", url); });
+            }
+
+            // iterate over the list
+            for (Element element : filteredElements) {
+                String id = element.attr("href").substring(6);
+                String url = project + element.attr("href");
+                WikiPage wp = WikiPage.builder().
+                        project(project).
+                        category(category).
+                        url(url).
+                        id(id).
+                        title(element.text()).
+                        views(WikiPageStats.retrieveWikiPageStats(id)).
+                        build();
+
+                pages.add(wp);
+            }
+
+            return pages;
+        }
+    }
+
+    /**
+     * Representation of wiki page with the number of views per page.
+     * A page rank is according to the number of views.
+     */
+    @Data
+    @Builder
+    public static class WikiPage implements Comparable<WikiPage> {
+        private String project;
+        private String category;
+        private String url;
+        private String id;
+        private String title;
+        private Integer views;
+
+        public static String header() {
+            return "url" + '|'  + "id" + '|' + "title" + '|' + "views";
+        }
+
+        @Override
+        public String toString() {
+            Assert.assertFalse( "Project is incorrect: " + category, category == null || category.contains("|"));
+            Assert.assertFalse("Category is incorrect: " + project, project == null || project.contains("|"));
+            Assert.assertFalse(     "Url is incorrect: " + url, url == null || url.contains("|"));
+            Assert.assertFalse(      "Id is incorrect: " + id, id == null || id.contains("|"));
+            Assert.assertFalse(   "Title is incorrect: " + title, title == null || title.contains("|"));
+            Assert.assertFalse("Views is incorrect: " + views, views == null);
+            return  project + '|'  +
+                    category + '|' +
+                    url + '|' +
+                    id + '|' +
+                    title + '|' +
+                    views;
+        }
+
+        public static WikiPage fromString(String line) {
+            String[] tokens = StringUtils.split(line, '|');
+            Assert.assertEquals("Line has " + tokens.length + " while 6 are expected: " + line, 6, tokens.length);
+            try {
+                Integer views = Integer.parseInt(tokens[5]);
+                return WikiPage.builder().project(tokens[0]).category(tokens[1]).url(tokens[2]).id(tokens[3]).title(tokens[4]).views(views).build();
+            } catch (NumberFormatException e) {
+                throw new RuntimeException("Number of views '" + tokens[5] + "' is not integer: " + line, e);
+            }
+        }
+
+        @Override
+        public int compareTo(WikiPage another) {
+            return views.compareTo(another.views);
+        }
+    }
+
+
+    /**
+     * Popular pages: https://tools.wmflabs.org/pageviews/?project=en.wikipedia.org&platform=all-access&agent=user&range=latest-20&pages=Benjamin_Zephaniah|Lord_Byron|E._F._Benson
+     */
+    public static class WikiPageStats {
+        private static final Logger log = getLogger(WikiPageStats.class);
+        public static final int MAX_ERROR_RESOLUTION_ATTEMPTS = 3;
+        private static String fromDate = "20150904";
+        private static String toDate = "20160903";
+
+        public static Integer retrieveWikiPageStats(String pageId) {
+            try (CloseableHttpClient httpClient = HttpClientBuilder.create().build()) {
+                Object resultObject = requestPageStats(httpClient, pageId);
+                log.debug(resultObject);
+                Integer views = calculateTotalNumberOfViews(resultObject);
+                if (views == 0) {
+                    int attempts = 0;
+                    while (resultObject instanceof JSONObject && ((JSONObject) resultObject).containsKey("type") && attempts< MAX_ERROR_RESOLUTION_ATTEMPTS) {
+                        Thread.sleep(1000); // Give server a break
+                        log.debug("Re-executing request for [{}] because of an error: {}", pageId, resultObject);
+                        resultObject = requestPageStats(httpClient, pageId);
+                        views = calculateTotalNumberOfViews(resultObject);
+                        attempts++;
+                    }
+                    if (views == 0) {
+                        log.warn("Zero views according to the response: {}", resultObject);
+                    }
+                }
+                log.info("Number of views for page [{}] through [{}, {}]: {}", pageId, fromDate, toDate, views);
+                return views;
+            } catch (Exception e) {
+                throw new RuntimeException("Could not retrieve stat for page #" + pageId, e);
+            }
+        }
+
+        private static Integer calculateTotalNumberOfViews(Object resultObject) {
+            return filterTraverse(resultObject, jsonObject -> jsonObject.containsKey("views")).
+                                stream().map(jsonObject -> Integer.parseInt(valueOf(jsonObject.get("views")))).collect(summingInt(Integer::intValue));
+        }
+
+        private static Object requestPageStats(CloseableHttpClient httpClient, String pageId) throws IOException, ParseException {
+            HttpGet request = new HttpGet(format("https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/en.wikipedia/all-access/user/%s/daily/%s00/%s00", pageId, fromDate, toDate));
+            request.addHeader("content-type", "application/json");
+            HttpResponse result = httpClient.execute(request);
+            String json = EntityUtils.toString(result.getEntity(), "UTF-8");
+            JSONParser parser = new JSONParser();
+            return parser.parse(json);
+        }
+
+        private static ArrayList<JSONObject> filterTraverse(Object jsonObject, Predicate<JSONObject> filter) {
+            ArrayList<JSONObject> returnedObjects = new ArrayList<>();
+            filterInternalTraverse(jsonObject, filter, returnedObjects);
+            return returnedObjects;
+        }
+
+        private static void filterInternalTraverse(Object jsonObject, Predicate<JSONObject> filter, List<JSONObject> returnedObjects) {
+            if (jsonObject instanceof JSONObject) {
+                JSONObject obj = (JSONObject) jsonObject;
+                if (filter.test(obj)) {
+                    returnedObjects.add(obj);
+                }
+                for (Object key : obj.keySet()) {
+                    filterInternalTraverse(obj.get(key), filter, returnedObjects);
+                }
+            } else if (jsonObject instanceof JSONArray) {
+                JSONArray array = (JSONArray) jsonObject;
+                for (Object object : array) {
+                    filterInternalTraverse(object, filter, returnedObjects);
+                }
+            }
+        }
+    }
+}
